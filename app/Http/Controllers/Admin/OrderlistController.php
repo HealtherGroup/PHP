20e8a6501955908b192101eb52ede63f1a61b065
zhang <1451722738@qq.com>
2016-12-29 09:12:02 +0800

    2016-12-29 9:11:59

--- app/Http/Controllers/Admin/OrderlistController.php
+++ app/Http/Controllers/Admin/OrderlistController.php
@@ -2,6 +2,7 @@
 namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Requests;
+use DB;
 use App\Http\Controllers\Controller;
 use App\Service\Admin\RoleService;
 use App\Http\Requests\RoleRequest;
@@ -9,8 +10,25 @@ class OrderlistController extends Controller
 {
     private $orderlist;
 
-    public function index()
+    public function index(Request $request)
     {
-        return view('admin.homeorder.list');
+        $uid = $request->session()->get('userid');
+        $res=DB::table('user')->where('id',$uid)->first();
+     
+        if($uid == null){
+            return redirect('/Pleaselogin');
+        }
+        $list = \DB::table('order')->where('uid',$uid)->orderBy('id','desc')->get();
+      
+        return view('admin.homeorder.list1',['list' => $list],['res'=>$res]);
+    }
+    public function detailindex(Request $request)
+    {
+        $oid=$request->get('oid');
+        $username=$request->get('username');
+        echo $oid;
+        echo $username;
+        die;
+          $orderdata = \DB::table('orderdata')->get();
     }
 }
